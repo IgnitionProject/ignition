b8cd201aef68bd4f6821bdb38a7872d13a82d9f0
Andy R. Terrel <andy.terrel@gmail.com>
2011-01-06 16:35:30 -0600

    Fix github pages script.

--- doc/fix_for_gh_subdirs.sh
+++ doc/fix_for_gh_subdirs.sh
@@ -4,9 +4,11 @@ set -e
 
 WDIR=$PWD
 cd build/html
+mv _modules modules_gpf
 mv _sources sources
 mv _static static
-grep -r _static * | cut -f 1 -d : | xargs sed -i "bk" "s/_static/static/g"
+grep -r _modules * | cut -f 1 -d : | xargs sed -i "bk" "s/_modules/modules_gpf/g"
 grep -r _sources * | cut -f 1 -d : | xargs sed -i "bk" "s/_sources/sources/g"
+grep -r _static * | cut -f 1 -d : | xargs sed -i "bk" "s/_static/static/g"
 rm *bk */*bk
 cd $WDIR
