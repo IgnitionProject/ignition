c21304dea54c0627b13f6b4c32cf5f262846fa58
Andy R. Terrel <andy.terrel@gmail.com>
2011-02-24 13:56:19 -0600

    Fixing issue with solutions file in cg demo.

--- demo/iterative/CG.py
+++ demo/iterative/CG.py
@@ -79,4 +79,4 @@ O = iterative_arg ("O", rank=2, part_suffix="1x3", arg_src="Computed")
 # Generate the algorithm
 generate(op=CG_Op, loop_inv=CG_Inv, solver=tensor_solver,
          inv_args=[A, X, P, I, U, J, D, R, O], filetype="latex",
-         solution_file="/Users/aterrel/workspace/flame/tacc/flame-it/ignition/cg_updates.tex")
+         solution_file="cg_updates.tex")
