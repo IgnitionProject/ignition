bee72748dad9bbad3b567f3885e50065c1d44ac1
Andy R. Terrel <andy.terrel@gmail.com>
2013-05-05 15:24:36 -0500

    Organizing language file

--- ignition/sfl/language.py
+++ ignition/sfl/language.py
@@ -14,10 +14,18 @@ class Time(Variable):
         obj = Unknown.__new__(cls, 'time', space="L2")
         return obj
 
+# Function space definition
 class FunctionSpace(Symbol):
     """Represents a function space"""
     pass
 
+# Measures
+class Measure(Symbol):
+    pass
+
+dX = Measure('dX')
+
+# Domains
 class Domain(Symbol):
     """Represents a domain"""
     pass
@@ -26,12 +34,14 @@ class Region(Domain):
     """Represents a region (or subset of a domain)"""
     pass
 
+# Functions
 class Function(Symbol):
     pass
 
+
+# Operators
 class Operator(Expr):
     pass
-
 
 class div(Operator):
     pass
@@ -53,20 +63,23 @@ class rot(Operator):
     pass
 
 
+# Coefficients
+class Coefficient(Symbol):
+    """Represents a coefficient that is evaluated at quadrature points.
 
-class Coefficient(object):
-    pass
+    Expression that is not defined by unknowns from another SFL
+    expr. Code linking to the generated code must defind the named
+    function for evaluation.
+    """
+    def __new__(cls, name, rank):
+        obj = Symbol.__new__(cls, name)
+        obj.rank = rank
+        return obj
 
-class Constant(object):
-    pass
+class Constant(Coefficient):
+    """Represents a constant coefficient that is evaluated at quadrature points.
 
-class VectorConstant(object):
-    pass
-
-class TensorConstant(object):
-    pass
-
-class Measure(Symbol):
-    pass
-
-dX = Measure('dX')
+    Expression that is not defined by unknowns from another SFL expr.
+    """
+    def __new__(cls, val, rank=None):
+        return Coefficient.__new__(cls, rank)
