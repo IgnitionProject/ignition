93435f2a8c7a2d1b8e35d27db23db81a1dc9ff8a
Andy R. Terrel <andy.terrel@gmail.com>
2013-09-24 16:37:37 -0500

    Adding Python code printer

--- ignition/code_tools/tests/test_code_printer.py
+++ ignition/code_tools/tests/test_code_printer.py
@@ -2,7 +2,7 @@ from importlib import import_module
 import sys
 
 from ignition.code_tools.code_obj import Statement
-from ignition.code_tools.code_printer import CCodePrinter
+from ignition.code_tools.code_printer import CCodePrinter, PythonCodePrinter
 
 from test_code_obj import create_sum_squares, create_double_sum, create_index_variable_loop
 
@@ -12,28 +12,57 @@ def test_statement_C():
     printer = CCodePrinter(dag)
     assert(printer.code_str() == "a = b;\n")
 
+
 def test_sum_of_squares_C():
-    """Test for compiling \sum_{i=1}^N i*i"""
+    """Test for C compiling \sum_{i=1}^N i*i"""
     dag = create_sum_squares()
     cp = CCodePrinter(dag)
-    module_name = "sum_of_squares"
+    module_name = "ignition_sum_of_squares_c"
     module_path = cp.to_ctypes_module(module_name)
     sys.path.append(module_path)
     sum_of_squares = import_module(module_name)
     assert(sum_of_squares.sum_squares(4) == 30)
 
+
 def test_double_sum_C():
     dag = create_double_sum()
-    modname = "double_sum"
+    modname = "ignition_double_sum_c"
     modpath = CCodePrinter(dag).to_ctypes_module(modname)
     sys.path.append(modpath)
     double_sum = import_module(modname)
     assert(double_sum.double_sum(3) == 20)
 
-def test_jndex_variable_loop_C():
+
+def test_index_variable_loop_C():
     dag = create_index_variable_loop()
-    modname = "index_variable_loop"
+    modname = "ignition_index_variable_loop_c"
     modpath = CCodePrinter(dag).to_ctypes_module(modname)
     sys.path.append(modpath)
     index_variable_loop = import_module(modname)
     assert(index_variable_loop.idx_loop_fun() == 45)
+
+
+def test_statement_Py():
+    dag = Statement("=", 'a', 'b')
+    printer = PythonCodePrinter(dag)
+    assert(printer.code_str() == "a = b\n")
+
+
+def test_sum_of_squares_Py():
+    """Test for Python running \sum_{i=1}^N i*i"""
+    dag = create_sum_squares()
+    cp = PythonCodePrinter(dag)
+    module_name = "ignition_sum_of_squares_py"
+    module_path = cp.to_module(module_name)
+    sys.path.append(module_path)
+    sum_of_squares = import_module(module_name)
+    assert(sum_of_squares.sum_squares(4) == 30)
+
+
+def test_double_sum_Py():
+    dag = create_double_sum()
+    modname = "ignition_double_sum_py"
+    modpath = PythonCodePrinter(dag).to_module(modname)
+    sys.path.append(modpath)
+    double_sum = import_module(modname)
+    assert(double_sum.double_sum(3) == 20)
