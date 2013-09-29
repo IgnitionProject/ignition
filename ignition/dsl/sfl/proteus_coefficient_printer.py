caf0829191e9f3276fb144486ad602dcd482b60d
Andy R. Terrel <andy.terrel@gmail.com>
2013-09-29 16:40:05 -0500

    Print head, remove code for proteus python class head (use codeobj)

--- ignition/dsl/sfl/proteus_coefficient_printer.py
+++ ignition/dsl/sfl/proteus_coefficient_printer.py
@@ -8,20 +8,20 @@ coefficient_header = """\
 Proteus Coefficient file generated from Ignition
 """
 
-class_header = """\
-class %{class_name}s(TC_base):
-"""
-
 
 class ProteusCoefficientPrinter(SFLPrinter):
     """Generator for Proteus Coefficient evaluator"""
 
     language = 'Python'
     comment_str = '//'
-    block_comment_tuple = ('"""', '"""')
+    block_comment_tuple = ('"""', '"""\n')
+
+    def _print_header(self, indent):
+        return comment_code(indent_code(coefficient_header, indent),
+                            block_comment=self.block_comment_tuple)
 
     def print_file(self, indent=0):
         ret_code = ""
         ret_code += self._print_header(indent)
-        ret_code += PythonCodePrinter(self._generator.class_dag)
+        ret_code += PythonCodePrinter(self._generator.class_dag).code_str()
         return ret_code
