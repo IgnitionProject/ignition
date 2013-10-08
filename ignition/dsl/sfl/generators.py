2515be7eaae5787ba692a3cf4f2256263b1e7197
Andy R. Terrel <andy.terrel@gmail.com>
2013-10-08 17:05:32 -0500

    Adding script generator

--- ignition/dsl/sfl/generators.py
+++ ignition/dsl/sfl/generators.py
@@ -168,6 +168,44 @@ if self.rFunc != None:
             f.write(printer.print_file())
 
 
+class ProteusScriptGenerator(SFLGenerator):
+    """Generates a base script for calling a SFL code from Proteus"""
+    def __init__(self, expr, **kwargs):
+        super(ProteusScriptGenerator, self).__init__(expr, **kwargs)
+        self.expr = expr
+        self.coefficient_class = kwargs.get("coefficient_class")
+        self.module_name = kwargs.get("module_name")
+        if self.coefficient_class is None:
+            self._gen_coefficient_class()
+        self._filename = None
+        self._classname = None
+
+    @property
+    def filename(self):
+        if self._filename is None:
+            # Try to get filename from keywords
+            if "filename" in self.kwargs:
+                self._filename = self.kwargs["filename"]
+            else:
+                # Get filename from script name
+                self._filename = os.path.split(sys.argv[0])[1][:-3]+"_proteus_script.py"
+        return self._filename
+
+    def _gen_coefficient_class(self):
+        coefficient_generator = ProteusCoefficientGenerator(self.expr)
+        coefficient_generator.to_file()
+        self.module_name = coefficient_generator.filename
+        self.coefficient_class = coefficient_generator.classname
+
+    def to_file(self, filename=None):
+        printer = ProteusScriptPrinter(self)
+        if filename is not None:
+            self._filename = filename
+        with open(self.filename, 'w') as f:
+            print("Writing proteus script file to %s" % self.filename)
+            f.write(printer.print_file())
+
+
 class UFLGenerator(SFLGenerator):
     """SFL generator for UFL language"""
     def to_file(self, filename):
@@ -177,5 +215,7 @@ class UFLGenerator(SFLGenerator):
 
 def generate(framework, expr, **kwargs):
     """Generates the equation in a lower level framework"""
-    if framework == "proteus":
+    if framework == "proteus-coefficient":
         return ProteusCoefficientGenerator(expr, **kwargs).to_file()
+    elif framework == "proteus-script":
+        return ProteusScriptGenerator(expr, **kwargs).to_file()
