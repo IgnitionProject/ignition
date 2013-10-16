53efd4473e93dcb43c95fa3d3f954a044485d98e
Andy R. Terrel <andy.terrel@gmail.com>
2013-10-16 15:46:09 -0500

    Adding module printing to code printer

--- ignition/dsl/sfl/proteus_coefficient_printer.py
+++ ignition/dsl/sfl/proteus_coefficient_printer.py
@@ -16,6 +16,11 @@ class ProteusCoefficientPrinter(SFLPrinter):
     comment_str = '//'
     block_comment_tuple = ('"""', '"""\n')
 
+    def __init__(self, generator):
+        super(ProteusCoefficientPrinter, self).__init__(generator)
+        self._printer = PythonCodePrinter(self._generator.class_dag,
+                                          self._generator._modules)
+
     def _print_header(self, indent):
         return comment_code(indent_code(coefficient_header, indent),
                             block_comment=self.block_comment_tuple)
@@ -23,9 +28,5 @@ class ProteusCoefficientPrinter(SFLPrinter):
     def print_file(self, indent=0):
         ret_code = ""
         ret_code += self._print_header(indent)
-        if self._generator._modules:
-            for mod_str in self._generator._modules:
-                ret_code += mod_str + "\n"
-            ret_code += "\n"
-        ret_code += PythonCodePrinter(self._generator.class_dag).code_str()
+        ret_code += self._printer.code_str()
         return ret_code
